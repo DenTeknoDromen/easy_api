fcc9164aef20150bb76d2cf8339b138cd06848e2
Tobias Jansson <tobias.janssn@gmail.com>
2024-12-11 14:25:17 +0100

    feat: Added docker compose

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { collectDefaultMetrics, register } = require ('prom-client')
 collectDefaultMetrics();
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.use(express.json());
 
